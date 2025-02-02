df37c6359376af93cd8d31da969755377236e86d
IbrahimElmahy <144167714+IbrahimElmahy@users.noreply.github.com>
2025-02-02 07:14:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Full Stack .Net Developer','Frontend Developer','Backend Developer'],
+    strings: ['Full Stack .Net Developer','Graphic Designer','Database Administrator'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
